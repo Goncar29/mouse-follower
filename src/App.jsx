4b4804371d1db47076603775a9c4fe08c7417007
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-07-11 14:47:35 -0300

    se crea la logica para el puntero y los estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
+    const [enabled, setEnabled] = useState(false)
+
+    useEffect(() => {
+        console.log('effect', { enabled })
+
+        const handleMove = (event) => {
+            const { clientX, clientY } = event
+            console.log('handleMove', { clientX, clientY })
+        }
+
+        if (enabled) {
+            window.addEventListener('pointermove', handleMove)
+        }
+        
+    }, [enabled])
 
     return (
-        <h1>
-            Mouse Follower
-        </h1>
+        <main>
+        <div style={{ 
+            position: 'absolute',
+            backgroundColor: '#09f',
+            borderRadius: '50%',
+            opacity: 0.8,
+            pointerEvents: 'none',
+            left: -20,
+            top: -20,
+            width: '40',
+            height: '40',
+            transform: 'translate(0px, 0px)',
+        }}>
+        </div>
+            <h1>
+                Mouse Follower
+            </h1>
+            <button onClick={() => setEnabled(!enabled)}>
+                {enabled ? 'Desactivar' : 'Activar'} puntero
+            </button>
+        </main>
     )
 }
 
