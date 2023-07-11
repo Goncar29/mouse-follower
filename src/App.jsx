9cfccf1d036da5379ea2c7e06635cb34d3423df9
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-07-11 15:16:25 -0300

    se crea un nuevo estado y un efecto nuevo, se crea otra funcion separando del App

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,76 @@
 import './App.css'
 import { useEffect, useState } from 'react'
 
-function App() {
+const FollowMouse = () => {
     const [enabled, setEnabled] = useState(false)
+    const [position, setPosition] = useState({ x: 0, y: 0 })
 
+    //pinter move
     useEffect(() => {
-        console.log('effect', { enabled })
+        console.log('effect ', { enabled })
 
         const handleMove = (event) => {
             const { clientX, clientY } = event
             console.log('handleMove', { clientX, clientY })
+            setPosition({ x: clientX, y: clientY })
         }
 
         if (enabled) {
             window.addEventListener('pointermove', handleMove)
         }
-        
+        // cleanup:
+        // Esto se ejecuta cuando el componente se desmonta
+        // y cuando cambia las dependencias, antes de ejecutar el efecto de nuevo
+        return () => {
+            console.log('cleanup')
+            window.removeEventListener('pointermove', handleMove)
+        }
+    }, [enabled]) // dependencia
+
+    // [] -> solo se ejecuta una vez cuando se monta el componente
+    // [enabled] -> se ejecuta cuando cambia enabled y cuando se monta el componente
+    // undefined -> se ejecuta cada vez que se renderiza el componente
+
+    // change body className
+    useEffect(() => {
+        document.body.classList.toggle('no-cursor', enabled)
+
+        return () => {
+        document.body.classList.remove('no-cursor')
+        }
     }, [enabled])
 
+
     return (
-        <main>
+        <>
         <div style={{ 
             position: 'absolute',
-            backgroundColor: '#09f',
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            border: '1px solid #fff',
             borderRadius: '50%',
             opacity: 0.8,
             pointerEvents: 'none',
-            left: -20,
-            top: -20,
-            width: '40',
-            height: '40',
-            transform: 'translate(0px, 0px)',
-        }}>
-        </div>
+            left: -25,
+            top: -25,
+            width: '50px',
+            height: '50px',
+            transform: `translate(${position.x}px, ${position.y}px)`,
+        }} />
             <h1>
                 Mouse Follower
             </h1>
             <button onClick={() => setEnabled(!enabled)}>
                 {enabled ? 'Desactivar' : 'Activar'} puntero
             </button>
+        </>
+    )
+}
+
+
+function App() {
+    return (
+        <main>
+            <FollowMouse />
         </main>
     )
 }
